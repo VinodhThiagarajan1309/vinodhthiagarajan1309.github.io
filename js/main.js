6d4448118b1e82c80269b9c83376998d8dc6c0b6
Vinodh Thiagarajan <vinodhthiagarajan@Vinodhs-MacBook-Pro.local>
2016-04-30 19:29:18 -0500

    Final Commit

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
 
 $('document').ready(function(){
 
-	$(function() {
-  $('.target').hovercap({
-    toggleElement: '.example__caption',
-    toggleAnimataion: 'fade'
-  });
-});
+
+	$('#img1').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+	$('#img2').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+	$('#img3').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+	$('#img4').contenthover({
+			overlay_background:'#000',
+			overlay_opacity:0.8
+		});
+
+	
+
 });
 
-$(function() {
-  $('.target').hovercap({
-    toggleElement: '.example__caption',
-    toggleAnimataion: 'fade'
-  });
-});
